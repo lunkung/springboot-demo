b08ca2d631ededcd11a50b351e4b6b0203e593d4
lunkung <1522835776@qq.com>
2018-12-06 01:10:27 +0800

    add

---
+++ springboot-web-thymeleaf-1/src/main/java/com/atlunk/springboot/SpringbootWebThymeleaf1Application.java
@@ -0,0 +1,12 @@
+package com.atlunk.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebThymeleaf1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWebThymeleaf1Application.class, args);
+	}
+}
