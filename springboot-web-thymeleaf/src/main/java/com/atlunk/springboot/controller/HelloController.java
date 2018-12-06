4274a16476521c06fad270081ee069d3dfd1ec1a
lunkung <1522835776@qq.com>
2018-12-06 22:45:45 +0800

    update thymeleaf and document

--- springboot-web-thymeleaf/src/main/java/com/atlunk/springboot/controller/HelloController.java
+++ springboot-web-thymeleaf/src/main/java/com/atlunk/springboot/controller/HelloController.java
@@ -1,5 +1,8 @@
 package com.atlunk.springboot.controller;
 
+import java.util.Arrays;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -20,8 +23,10 @@ public class HelloController {
 	}
 	
 	@RequestMapping("/success")
-	public String success() {
+	public String success(Map<String, Object> map) {
 		//classpath:/templates/success.html
+		map.put("hello", "<h1>你好</h1>");
+		map.put("users", Arrays.asList("zhansan", "lisi", "wangwu"));
 		return "success";
 	}
 	
